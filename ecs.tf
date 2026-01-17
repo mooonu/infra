563d29b8fae2f9fbf86756d00f61dac7b49be909
mooonu <moonudev@gmail.com>
2026-01-17 20:08:35 +0900

    feat: ECS Exec 기능 활성화

--- ecs.tf
+++ ecs.tf
@@ -89,6 +89,7 @@ resource "aws_ecs_service" "api" {
   task_definition = aws_ecs_task_definition.api.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+  enable_execute_command = true
 
   network_configuration {
     subnets = [
--- iam.tf
+++ iam.tf
@@ -112,6 +112,30 @@ resource "aws_iam_role_policy" "api_kvs_s3" {
   )
 }
 
+resource "aws_iam_role_policy" "api_exec" {
+  name = "qwik-api-exec-policy"
+  role = aws_iam_role.ecs_task.id
+
+  policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "ssmmessages:CreateControlChannel",
+            "ssmmessages:CreateDataChannel",
+            "ssmmessages:OpenControlChannel",
+            "ssmmessages:OpenDataChannel"
+          ]
+          Resource = "*"
+        }
+      ]
+    }
+  )
+}
+
+
 # -- ECS Worker Task Role
 resource "aws_iam_role" "ecs_worker_task" {
   name               = "qwik-ecs-worker-task-role"
